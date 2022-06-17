23f4e09a4d900a861a0de08c43f03ced76e5fb72
Fabricio Quintas <104725578+FabricioQuintas@users.noreply.github.com>
2022-06-17 14:42:05 -0300

    Week 1

--- Assignment 06.5 (Python Data Structures).py
+++ Assignment 06.5 (Python Data Structures).py
@@ -1,4 +1,4 @@
-### Assignment 06.5 (Week 1)
+### Assignment 6.5 (Week 1)
 # Write code using find() and string slicing (see section 6.10) to extract the number at the end of the line below.
 # Convert the extracted value to a floating point number and print it out.
 
