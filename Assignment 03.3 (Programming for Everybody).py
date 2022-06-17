c7d8d31bb3bd5e5b53634038172def2161256566
Fabricio Quintas <104725578+FabricioQuintas@users.noreply.github.com>
2022-06-17 14:40:30 -0300

    Week 5

--- Assignment 03.3 (Programming for Everybody).py
+++ Assignment 03.3 (Programming for Everybody).py
@@ -1,4 +1,4 @@
-### Assignment 3.3 (Week 5) 
+### Assignment 03.3 (Week 5) 
 # Write a program to prompt for a score between 0.0 and 1.0. 
 # If the score is out of range, print an error. 
 # If the score is between 0.0 and 1.0, print a grade using the following table:
